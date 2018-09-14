874396b7260dd2fa70d1515a8e8dde91623a1c33
Kees Cook <keescook@chromium.org>
2018-09-13 22:28:48 -0700

    UPSTREAM: LSM: Plumb visibility into optional "enabled" state
    In preparation for lifting the "is this LSM enabled?" logic out of the
    individual LSMs, pass in any special enabled state tracking (as needed
    for SELinux, AppArmor, and LoadPin). This should be an "int" to include
    handling any future cases where "enabled" is exposed via sysctl which
    has no "bool" type.
    
    Change-Id: I9b09ced67902875d1aebe9435b59672d1b882e41
    Signed-off-by: Kees Cook <keescook@chromium.org>
    Reviewed-by: Casey Schaufler <casey@schaufler-ca.com>
    Reviewed-by: John Johansen <john.johansen@canonical.com>
    Signed-off-by: Naveen <133593113+elohim-etz@users.noreply.github.com>

--- security/selinux/hooks.c
+++ security/selinux/hooks.c
@@ -7063,6 +7063,7 @@ int get_current_security_context(char **context, u32 *context_len)
 DEFINE_LSM(selinux) = {
 	.name = "selinux",
 	.flags = LSM_FLAG_LEGACY_MAJOR,
+	.enabled = &selinux_enabled,
 	.init = selinux_init,
 };
 
