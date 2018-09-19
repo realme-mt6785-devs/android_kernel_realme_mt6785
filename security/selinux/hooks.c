9b9eba6786aa908d058769c224bdd0b68ab9e216
Kees Cook <keescook@chromium.org>
2018-09-19 16:13:25 -0700

    UPSTREAM: LSM: Introduce LSM_FLAG_LEGACY_MAJOR
    This adds a flag for the current "major" LSMs to distinguish them when
    we have a universal method for ordering all LSMs. It's called "legacy"
    since the distinction of "major" will go away in the blob-sharing world.
    
    Change-Id: Ie5636e4425bcbdad37da78ec7c045404f5b2ccda
    Signed-off-by: Kees Cook <keescook@chromium.org>
    Reviewed-by: Casey Schaufler <casey@schaufler-ca.com>
    Reviewed-by: John Johansen <john.johansen@canonical.com>
    Signed-off-by: Naveen <133593113+elohim-etz@users.noreply.github.com>

--- security/selinux/hooks.c
+++ security/selinux/hooks.c
@@ -7062,6 +7062,7 @@ int get_current_security_context(char **context, u32 *context_len)
    all processes and objects when they are created. */
 DEFINE_LSM(selinux) = {
 	.name = "selinux",
+	.flags = LSM_FLAG_LEGACY_MAJOR,
 	.init = selinux_init,
 };
 
