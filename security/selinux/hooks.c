726e1ef2476ff091e48fad6ecf685c8fc58bd15f
Kees Cook <keescook@chromium.org>
2018-10-10 17:18:24 -0700

    UPSTREAM: LSM: Record LSM name in struct lsm_info
    In preparation for making LSM selections outside of the LSMs, include
    the name of LSMs in struct lsm_info.
    
    Change-Id: I129da3575cd955862cd00e139ee04d94a79bd1b4
    Signed-off-by: Kees Cook <keescook@chromium.org>
    Reviewed-by: Casey Schaufler <casey@schaufler-ca.com>
    Signed-off-by: James Morris <james.morris@microsoft.com>
    Signed-off-by: Naveen <133593113+elohim-etz@users.noreply.github.com>

--- security/selinux/hooks.c
+++ security/selinux/hooks.c
@@ -7067,6 +7067,7 @@ int get_current_security_context(char **context, u32 *context_len)
 /* SELinux requires early initialization in order to label
    all processes and objects when they are created. */
 DEFINE_LSM(selinux) = {
+	.name = "selinux",
 	.init = selinux_init,
 };
 
