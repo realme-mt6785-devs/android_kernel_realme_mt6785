f00b2901ffea6a86c52ee86eefbd9e5172bdf624
Tejun Heo <tj@kernel.org>
2018-12-28 10:31:07 -0800

    BACKPORT: cgroup: Add named hierarchy disabling to cgroup_no_v1 boot param
    It can be useful to inhibit all cgroup1 hierarchies especially during
    transition and for debugging.  cgroup_no_v1 can block hierarchies with
    controllers which leaves out the named hierarchies.  Expand it to
    cover the named hierarchies so that "cgroup_no_v1=all,named" disables
    all cgroup1 hierarchies.
    
    Signed-off-by: Tejun Heo <tj@kernel.org>
    Suggested-by: Marcin Pawlowski <mpawlowski@fb.com>
    Signed-off-by: Tejun Heo <tj@kernel.org>
    Change-Id: Ibd093dd9b70d15402a21db3c1ef56005ebc7f99e
    (cherry picked from commit 3fc9c12d27b4ded4f1f761a800558dab2e6bbac5)
    Bug: 154548692
    Signed-off-by: Marco Ballesio <balejs@google.com>
    Signed-off-by: Alexander Winkowski <dereference23@outlook.com>

--- kernel/cgroup/cgroup-v1.c
+++ kernel/cgroup/cgroup-v1.c
@@ -34,6 +34,9 @@
 /* Controllers blocked by the commandline in v1 */
 static u16 cgroup_no_v1_mask;
 
+/* disable named v1 mounts */
+static bool cgroup_no_v1_named;
+
 /*
  * pidlist destructions need to be flushed on cgroup destruction.  Use a
  * separate workqueue as flush domain.
@@ -1003,6 +1006,10 @@ static int parse_cgroupfs_options(char *data, struct cgroup_sb_opts *opts)
 		}
 		if (!strncmp(token, "name=", 5)) {
 			const char *name = token + 5;
+
+			/* blocked by boot param? */
+			if (cgroup_no_v1_named)
+				return -ENOENT;
 			/* Can't specify an empty name */
 			if (!strlen(name))
 				return -EINVAL;
@@ -1348,7 +1355,12 @@ static int __init cgroup_no_v1(char *str)
 
 		if (!strcmp(token, "all")) {
 			cgroup_no_v1_mask = U16_MAX;
-			break;
+			continue;
+		}
+
+		if (!strcmp(token, "named")) {
+			cgroup_no_v1_named = true;
+			continue;
 		}
 
 		for_each_subsys(ss, i) {
