0ef8f1041e78ae4f1829681a8df8f87c7e096934
Sultan Alsawaf <sultan@kerneltoast.com>
2020-12-25 21:39:23 -0800

    treewide: Suppress overly verbose log spam
    This tames quite a bit of the log spam and makes dmesg readable.
    
    Uses work from Danny Lin <danny@kdrag0n.dev>.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: officialputuid <officialputuid@hack.id>
    Signed-off-by: Naveen <133593113+elohim-etz@users.noreply.github.com>

--- kernel/irq/cpuhotplug.c
+++ kernel/irq/cpuhotplug.c
@@ -170,7 +170,7 @@ void irq_migrate_all_off_this_cpu(void)
 #ifndef OPLUS_FEATURE_CHG_BASIC
 // Nanwei.Deng@BSP.CHG.Basic, 2018/07/13  Add for delete log in release version
 		if (affinity_broken) {
-			pr_warn_ratelimited("IRQ %u: no longer affine to CPU%u\n",
+			pr_debug_ratelimited("IRQ %u: no longer affine to CPU%u\n",
 					    irq, smp_processor_id());
 		}
 #else
