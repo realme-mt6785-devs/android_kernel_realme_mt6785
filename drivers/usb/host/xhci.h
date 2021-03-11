958abd02d44963ea8b344a4e0c9a2f1898c76fa4
Stanislaw Gruszka <stf_xl@wp.pl>
2021-03-11 13:53:50 +0200

    usb: xhci: do not perform Soft Retry for some xHCI hosts
    commit a4a251f8c23518899d2078c320cf9ce2fa459c9f upstream.
    
    On some systems rt2800usb and mt7601u devices are unable to operate since
    commit f8f80be501aa ("xhci: Use soft retry to recover faster from
    transaction errors")
    
    Seems that some xHCI controllers can not perform Soft Retry correctly,
    affecting those devices.
    
    To avoid the problem add xhci->quirks flag that restore pre soft retry
    xhci behaviour for affected xHCI controllers. Currently those are
    AMD_PROMONTORYA_4 and AMD_PROMONTORYA_2, since it was confirmed
    by the users: on those xHCI hosts issue happen and is gone after
    disabling Soft Retry.
    
    [minor commit message rewording for checkpatch -Mathias]
    
    Fixes: f8f80be501aa ("xhci: Use soft retry to recover faster from transaction errors")
    Cc: <stable@vger.kernel.org> # 4.20+
    Reported-by: Bernhard <bernhard.gebetsberger@gmx.at>
    Tested-by: Bernhard <bernhard.gebetsberger@gmx.at>
    Signed-off-by: Stanislaw Gruszka <stf_xl@wp.pl>
    Signed-off-by: Mathias Nyman <mathias.nyman@linux.intel.com>
    Bugzilla: https://bugzilla.kernel.org/show_bug.cgi?id=202541
    Link: https://lore.kernel.org/r/20210311115353.2137560-2-mathias.nyman@linux.intel.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    (cherry picked from commit 2a28c0f457c5563cef9fbad803d9445eee0c1e05)
    [Vegard: fix conflict in context due to missing commit
     9b1f935ae4daefbc6ff8929526875d55b7a53121 ("usb: xhci: Workaround for S3
     issue on AMD SNPS 3.0 xHC").]
    Signed-off-by: Vegard Nossum <vegard.nossum@oracle.com>

--- drivers/usb/host/xhci.h
+++ drivers/usb/host/xhci.h
@@ -1850,6 +1850,7 @@ struct xhci_hcd {
 #define XHCI_RESET_PLL_ON_DISCONNECT	BIT_ULL(34)
 #define XHCI_SNPS_BROKEN_SUSPEND    BIT_ULL(35)
 #define XHCI_DEV_WITH_SYNC_EP	BIT_ULL(63)
+#define XHCI_NO_SOFT_RETRY	BIT_ULL(40)
 
 	unsigned int		num_active_eps;
 	unsigned int		limit_active_eps;
