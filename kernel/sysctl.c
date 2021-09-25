8e2af31583ee10f369da38f2b5b4c191cc11ffa6
Juhyung Park <qkrwngud825@gmail.com>
2021-09-25 19:49:17 +0900

    BACKPORT: sched: promote nodes out of CONFIG_SCHED_DEBUG
    - This patch was backported from [1].
    
    [1]: https://github.com/arter97/android_kernel_oneplus_sm8350/commit/0ed5b00f732e8ee71e8c10e9df169fbc80ca0819
    
    [cyberknight777: backport to 4.14]
    Signed-off-by: Juhyung Park <qkrwngud825@gmail.com>
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -343,7 +343,6 @@ static struct ctl_table sysctl_base_table[] = {
 	{ }
 };
 
-#ifdef CONFIG_SCHED_DEBUG
 static int min_sched_granularity_ns = 100000;		/* 100 usecs */
 static int max_sched_granularity_ns = NSEC_PER_SEC;	/* 1 second */
 static int min_wakeup_granularity_ns;			/* 0 usecs */
@@ -352,7 +351,6 @@ static int max_wakeup_granularity_ns = NSEC_PER_SEC;	/* 1 second */
 static int min_sched_tunable_scaling = SCHED_TUNABLESCALING_NONE;
 static int max_sched_tunable_scaling = SCHED_TUNABLESCALING_END-1;
 #endif /* CONFIG_SMP */
-#endif /* CONFIG_SCHED_DEBUG */
 
 #ifdef OPLUS_FEATURE_UIFIRST
 // XieLiujie@BSP.KERNEL.PERFORMANCE, 2020/05/25, Add for UIFirst
@@ -424,7 +422,6 @@ static struct ctl_table kern_table[] = {
 		.proc_handler	= proc_dointvec,
 },
 #endif
-#ifdef CONFIG_SCHED_DEBUG
 	{
 		.procname	= "sched_min_granularity_ns",
 		.data		= &sysctl_sched_min_granularity,
@@ -522,6 +519,7 @@ static struct ctl_table kern_table[] = {
 		.mode		= 0644,
 		.proc_handler	= proc_dointvec,
 	},
+#ifdef CONFIG_SCHED_DEBUG
 	{
 		.procname	= "sched_nr_migrate",
 		.data		= &sysctl_sched_nr_migrate,
