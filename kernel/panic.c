c9b8ecc850702dd3719781aefe4a456ed5f63d73
Marco Elver <elver@google.com>
2022-01-19 18:09:56 -0800

    BACKPORT: panic: use error_report_end tracepoint on warnings
    Introduce the error detector "warning" to the error_report event and use
    the error_report_end tracepoint at the end of a warning report.
    
    This allows in-kernel tests but also userspace to more easily determine
    if a warning occurred without polling kernel logs.
    
    [akpm@linux-foundation.org: add comma to enum list, per Andy]
    
    Link: https://lkml.kernel.org/r/20211115085630.1756817-1-elver@google.com
    Change-Id: Ia82127785563994f9a8b07a4c9e5c2483242f9f0
    Signed-off-by: Marco Elver <elver@google.com>
    Cc: Steven Rostedt <rostedt@goodmis.org>
    Cc: Ingo Molnar <mingo@redhat.com>
    Cc: Alexander Potapenko <glider@google.com>
    Cc: Petr Mladek <pmladek@suse.com>
    Cc: Luis Chamberlain <mcgrof@kernel.org>
    Cc: Wei Liu <wei.liu@kernel.org>
    Cc: Mike Rapoport <rppt@kernel.org>
    Cc: Arnd Bergmann <arnd@arndb.de>
    Cc: John Ogness <john.ogness@linutronix.de>
    Cc: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
    Cc: Alexander Popov <alex.popov@linux.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: Naveen <133593113+elohim-etz@users.noreply.github.com>

--- kernel/panic.c
+++ kernel/panic.c
@@ -30,6 +30,7 @@
 #include <linux/ratelimit.h>
 #include <soc/oppo/oppo_project.h>
 #include <linux/sysfs.h>
+#include <trace/events/error_report.h>
 
 #define PANIC_TIMER_STEP 100
 #define PANIC_BLINK_SPD 18
@@ -630,6 +631,7 @@ void __warn(const char *file, int line, void *caller, unsigned taint,
 		dump_stack();
 
 	print_oops_end_marker();
+	trace_error_report_end(ERROR_DETECTOR_WARN, (unsigned long)caller);
 
 	/* Just a warning, don't kill lockdep. */
 	add_taint(taint, LOCKDEP_STILL_OK);
