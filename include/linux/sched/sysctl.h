52d145a3fd1a26fa85db96dc358b4f7a34d4a72e
Sarthak Roy <sarthakroy2002@gmail.com>
2023-09-18 21:27:03 +0530

    treewide: Drop `OPLUS_FEATURE_HUNG_TASK_ENHANCE`
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>
    Change-Id: Ia9b0a927bb57e89df512738c0a4a721b7d894943
    Signed-off-by: Naveen <133593113+elohim-etz@users.noreply.github.com>

--- include/linux/sched/sysctl.h
+++ include/linux/sched/sysctl.h
@@ -11,9 +11,6 @@ extern int	     sysctl_hung_task_check_count;
 extern unsigned int  sysctl_hung_task_panic;
 extern unsigned long sysctl_hung_task_timeout_secs;
 extern int sysctl_hung_task_warnings;
-#ifdef CONFIG_OPLUS_FEATURE_HUNG_TASK_ENHANCE
-extern int sysctl_hung_task_selective_monitoring;
-#endif
 int proc_dohung_task_timeout_secs(struct ctl_table *table, int write,
 		void *buffer, size_t *lenp, loff_t *ppos);
 #else
