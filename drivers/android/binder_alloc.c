fd106408d39d301f4a5b24604ce6ca36cd4f5050
Carlos Llamas <cmllamas@google.com>
2023-12-01 17:21:33 +0000

    binder: fix async space check for 0-sized buffers
    commit 3091c21d3e9322428691ce0b7a0cfa9c0b239eeb upstream.
    
    Move the padding of 0-sized buffers to an earlier stage to account for
    this round up during the alloc->free_async_space check.
    
    Fixes: 74310e06be4d ("android: binder: Move buffer out of area shared with user space")
    Reviewed-by: Alice Ryhl <aliceryhl@google.com>
    Signed-off-by: Carlos Llamas <cmllamas@google.com>
    Link: https://lore.kernel.org/r/20231201172212.1813387-5-cmllamas@google.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    (cherry picked from commit 05088b886fea59cc827e5b5cedb66165cf532f72)
    Signed-off-by: Vegard Nossum <vegard.nossum@oracle.com>

--- drivers/android/binder_alloc.c
+++ drivers/android/binder_alloc.c
@@ -447,6 +447,10 @@ static struct binder_buffer *binder_alloc_new_buf_locked(
 		}
 	}
 #endif /*OPLUS_FEATURE_HANS_FREEZE*/
+
+	/* Pad 0-size buffers so they get assigned unique addresses */
+	size = max(size, sizeof(void *));
+
 	if (is_async &&
 	    alloc->free_async_space < size + sizeof(struct binder_buffer)) {
 		binder_alloc_debug(BINDER_DEBUG_BUFFER_ALLOC,
@@ -454,9 +458,6 @@ static struct binder_buffer *binder_alloc_new_buf_locked(
 			      alloc->pid, size);
 		return ERR_PTR(-ENOSPC);
 	}
-
-	/* Pad 0-size buffers so they get assigned unique addresses */
-	size = max(size, sizeof(void *));
 
 	while (n) {
 		buffer = rb_entry(n, struct binder_buffer, rb_node);
