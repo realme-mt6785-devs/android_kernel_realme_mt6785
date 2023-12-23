290316eba3758fd6a9417a2e241c524978d8d91b
officialputuid <officialputuid@hack.id>
2023-12-23 10:05:38 +0000

    ged: Adjust GED_KPI_DEFAULT_FPS_MARGIN to 3 * Improved performance tracking precision. * Taken from android_kernel_realme_RMX2001 Q-based.
    Signed-off-by: officialputuid <officialputuid@hack.id>
    Signed-off-by: Naveen <133593113+elohim-etz@users.noreply.github.com>

--- drivers/misc/mediatek/gpu/ged/src/ged_kpi.c
+++ drivers/misc/mediatek/gpu/ged/src/ged_kpi.c
@@ -65,7 +65,7 @@ EXPORT_SYMBOL(ged_kpi_PushAppSelfFcFp_fbt);
 #define GED_KPI_SEC_DIVIDER 1000000000
 #define GED_KPI_MAX_FPS 60
 /* set default margin to be distinct from FPSGO(0 or 3) */
-#define GED_KPI_DEFAULT_FPS_MARGIN 4
+#define GED_KPI_DEFAULT_FPS_MARGIN 3
 #define GED_KPI_CPU_MAX_OPP 0
 
 
