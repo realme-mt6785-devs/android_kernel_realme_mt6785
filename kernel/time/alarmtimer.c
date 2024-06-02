a67338ec904327c2fb4ca63c6d8accc24e420fa1
Wahid Khan <wahidzk0091@gmail.com>
2024-06-03 02:41:46 +0530

    kernel/time: alarmtimer: increase wakeup timeout margin
    1 is too aggressive, which can cause issues, so keep it to +5
    
    Signed-off-by: Wahid Khan <wahidzk0091@gmail.com>
    Signed-off-by: Naveen <133593113+elohim-etz@users.noreply.github.com>

--- kernel/time/alarmtimer.c
+++ kernel/time/alarmtimer.c
@@ -305,7 +305,7 @@ static int alarmtimer_suspend(struct device *dev)
 		return 0;
 
 	if (ktime_to_ns(min) < 2 * NSEC_PER_SEC) {
-		__pm_wakeup_event(ws, ktime_to_ms(min) + 1);
+		__pm_wakeup_event(ws, ktime_to_ms(min) + 5);
         #ifdef VENDOR_EDIT
 		//Nanwei.Deng@BSP.Power.Basic 2018/11/19, add for analysis power coumption. count alarm times.
 		alarmtimer_suspend_flag_clear();
