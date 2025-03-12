3d0c9ccdbf697eb0f7df135637621aae9b48f574
WangYuli <wangyuli@uniontech.com>
2025-03-12 13:06:50 +0800

    nvmet-fc: Remove unused functions
    commit 1b304c006b0fb4f0517a8c4ba8c46e88f48a069c upstream.
    
    The functions nvmet_fc_iodnum() and nvmet_fc_fodnum() are currently
    unutilized.
    
    Following commit c53432030d86 ("nvme-fabrics: Add target support for FC
    transport"), which introduced these two functions, they have not been
    used at all in practice.
    
    Remove them to resolve the compiler warnings.
    
    Fix follow errors with clang-19 when W=1e:
      drivers/nvme/target/fc.c:177:1: error: unused function 'nvmet_fc_iodnum' [-Werror,-Wunused-function]
        177 | nvmet_fc_iodnum(struct nvmet_fc_ls_iod *iodptr)
            | ^~~~~~~~~~~~~~~
      drivers/nvme/target/fc.c:183:1: error: unused function 'nvmet_fc_fodnum' [-Werror,-Wunused-function]
        183 | nvmet_fc_fodnum(struct nvmet_fc_fcp_iod *fodptr)
            | ^~~~~~~~~~~~~~~
      2 errors generated.
      make[8]: *** [scripts/Makefile.build:207: drivers/nvme/target/fc.o] Error 1
      make[7]: *** [scripts/Makefile.build:465: drivers/nvme/target] Error 2
      make[6]: *** [scripts/Makefile.build:465: drivers/nvme] Error 2
      make[6]: *** Waiting for unfinished jobs....
    
    Fixes: c53432030d86 ("nvme-fabrics: Add target support for FC transport")
    Signed-off-by: WangYuli <wangyuli@uniontech.com>
    Reviewed-by: Chaitanya Kulkarni <kch@nvidia.com>
    Reviewed-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Keith Busch <kbusch@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Ulrich Hecht <uli@kernel.org>
    Signed-off-by: Naveen <133593113+elohim-etz@users.noreply.github.com>

--- drivers/nvme/target/fc.c
+++ drivers/nvme/target/fc.c
@@ -153,20 +153,6 @@ struct nvmet_fc_tgt_assoc {
 	struct kref			ref;
 };
 
-
-static inline int
-nvmet_fc_iodnum(struct nvmet_fc_ls_iod *iodptr)
-{
-	return (iodptr - iodptr->tgtport->iod);
-}
-
-static inline int
-nvmet_fc_fodnum(struct nvmet_fc_fcp_iod *fodptr)
-{
-	return (fodptr - fodptr->queue->fod);
-}
-
-
 /*
  * Association and Connection IDs:
  *
