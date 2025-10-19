8eb320f3dee5bfa0a2342e71172d90544a2a686e
backslashxx <118538522+backslashxx@users.noreply.github.com>
2025-10-19 21:52:12 +0800

    KernelSU, selinux/avc: add slow_avc_audit hook
    since kprobes is broken on this kernel, manual hook is needed for avc log spoofing
    
    Signed-off-by: backslashxx <118538522+backslashxx@users.noreply.github.com>
    Signed-off-by: Naveen <133593113+elohim-etz@users.noreply.github.com>

--- security/selinux/avc.c
+++ security/selinux/avc.c
@@ -800,6 +800,10 @@ static void avc_audit_post_callback(struct audit_buffer *ab, void *a)
 }
 
 
+#ifdef CONFIG_KSU
+extern int ksu_handle_slow_avc_audit(u32 *tsid);
+#endif
+
 /* This is the slow part of avc audit with big stack footprint */
 noinline int slow_avc_audit(struct selinux_state *state,
 			    u32 ssid, u32 tsid, u16 tclass,
@@ -813,6 +817,9 @@ noinline int slow_avc_audit(struct selinux_state *state,
 	if (!denied)
 		return 0;
 
+#ifdef CONFIG_KSU
+	ksu_handle_slow_avc_audit(&tsid);
+#endif
 	if (!a) {
 		a = &stack_data;
 		a->type = LSM_AUDIT_DATA_NONE;
