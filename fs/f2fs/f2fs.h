4378ac08bed5d0f4f064d70b82b487c8cf2eba9b
neophyte <173499506+neophyte721@users.noreply.github.com>
2025-11-13 06:27:28 +0000

    fs/f2fs: bump DEF_CP_INTERVAL to 180
    * More power efficient
    
    Signed-off-by: Naveen <133593113+elohim-etz@users.noreply.github.com>

--- fs/f2fs/f2fs.h
+++ fs/f2fs/f2fs.h
@@ -224,7 +224,7 @@ enum {
 //#define DEF_MID_DISCARD_ISSUE_TIME	500	/* 500 ms, if device busy */
 //#define DEF_MAX_DISCARD_ISSUE_TIME	60000	/* 60 s, if no candidates */
 #define DEF_DISCARD_URGENT_UTIL		80	/* do more discard over 80% */
-#define DEF_CP_INTERVAL			60	/* 60 secs */
+#define DEF_CP_INTERVAL			180	/* 180 secs */
 /* VENDOR_EDIT shifei.ge@TECH.Storage.FS
  * 2019-10-15, add for oDiscard
  */
