e47e24a4a841cccef19f85acbb831b3585e42684
Naveen <133593113+elohim-etz@users.noreply.github.com>
2025-12-16 11:22:27 +0530

    Revert "kernel: bpf: devmap: Create __dev_map_alloc_node"
    This reverts commit a00a698e0b975ac416d6c6ac960c6efde6fb1b09.
    
    Signed-off-by: Naveen <133593113+elohim-etz@users.noreply.github.com>

--- kernel/bpf/devmap.c
+++ kernel/bpf/devmap.c
@@ -317,30 +317,6 @@ static int dev_map_delete_elem(struct bpf_map *map, void *key)
 	return 0;
 }
 
-static struct bpf_dtab_netdev *__dev_map_alloc_node(struct net *net,
-						    struct bpf_dtab *dtab,
-						    u32 ifindex,
-						    unsigned int idx)
-{
-	gfp_t gfp = GFP_ATOMIC | __GFP_NOWARN;
-	struct bpf_dtab_netdev *dev;
-
-	dev = kmalloc_node(sizeof(*dev), gfp, dtab->map.numa_node);
-	if (!dev)
-		return ERR_PTR(-ENOMEM);
-
-	dev->dev = dev_get_by_index(net, ifindex);
-	if (!dev->dev) {
-		kfree(dev);
-		return ERR_PTR(-EINVAL);
-	}
-
-	dev->bit = idx;
-	dev->dtab = dtab;
-
-	return dev;
-}
-
 static int dev_map_update_elem(struct bpf_map *map, void *key, void *value,
 				u64 map_flags)
 {
@@ -360,9 +336,19 @@ static int dev_map_update_elem(struct bpf_map *map, void *key, void *value,
 	if (!ifindex) {
 		dev = NULL;
 	} else {
-		dev = __dev_map_alloc_node(net, dtab, ifindex, i);
-		if (IS_ERR(dev))
-			return PTR_ERR(dev);
+		dev = kmalloc_node(sizeof(*dev), GFP_ATOMIC | __GFP_NOWARN,
+				   map->numa_node);
+		if (!dev)
+			return -ENOMEM;
+
+		dev->dev = dev_get_by_index(net, ifindex);
+		if (!dev->dev) {
+			kfree(dev);
+			return -EINVAL;
+		}
+
+		dev->bit = i;
+		dev->dtab = dtab;
 	}
 
 	/* Use call_rcu() here to ensure rcu critical sections have completed
