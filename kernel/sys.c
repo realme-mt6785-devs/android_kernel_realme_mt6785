717312ef64116a86fdc35a518deeff3d6a987763
ExtremeXT <extremextdev@gmail.com>
2026-01-25 15:22:19 +0200

    kernel: Bump spoof version to 5.10.239
    Change-Id: Ic3d74d99a021d90b41e3de98c314a2b582aa60c1
    Signed-off-by: ExtremeXT <extremextdev@gmail.com>
    Signed-off-by: Naveen <133593113+elohim-etz@users.noreply.github.com>

--- kernel/sys.c
+++ kernel/sys.c
@@ -1195,7 +1195,7 @@ SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 	    !strncmp(current->comm, "netbpfload", 10) ||
 	    !strncmp(current->comm, "netd", 4)) {
 		if (current_uid().val == 0) {
-			strcpy(tmp.release, "5.4.186");
+			strcpy(tmp.release, "5.10.239");
 			pr_debug("fake uname: %s/%d release=%s\n",
 				 current->comm, current->pid, tmp.release);
 		}
