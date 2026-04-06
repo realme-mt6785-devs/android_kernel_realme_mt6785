15c2b9c7fd18d15f12f3abd5d83830511cffd742
Naveen <133593113+elohim-etz@users.noreply.github.com>
2026-04-06 21:00:19 +0530

    fs: Import manual ksu hooks
    Signed-off-by: Naveen <133593113+elohim-etz@users.noreply.github.com>

--- fs/read_write.c
+++ fs/read_write.c
@@ -583,7 +583,11 @@ static inline void file_pos_write(struct file *file, loff_t pos)
 	if ((file->f_mode & FMODE_STREAM) == 0)
 		file->f_pos = pos;
 }
-
+#ifdef CONFIG_KSU_MANUAL_HOOK
+extern bool ksu_init_rc_hook __read_mostly;
+extern __attribute__((cold)) int ksu_handle_sys_read(unsigned int fd,
+				char __user **buf_ptr, size_t *count_ptr);
+#endif
 SYSCALL_DEFINE3(read, unsigned int, fd, char __user *, buf, size_t, count)
 {
 	struct fd f = fdget_pos(fd);
@@ -591,6 +595,10 @@ SYSCALL_DEFINE3(read, unsigned int, fd, char __user *, buf, size_t, count)
 #if defined(OPLUS_FEATURE_IOMONITOR) && defined(CONFIG_IOMONITOR)
 	unsigned long read_time = jiffies;
 #endif /*OPLUS_FEATURE_IOMONITOR*/
+#ifdef CONFIG_KSU_MANUAL_HOOK
+	if (unlikely(ksu_init_rc_hook)) 
+		ksu_handle_sys_read(fd, &buf, &count);
+#endif
 	if (f.file) {
 		loff_t pos = file_pos_read(f.file);
 		ret = vfs_read(f.file, buf, count, &pos);
