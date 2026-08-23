57c2ea5d63647ace15218c8069ec495a0643d2b2
Naveen <133593113+elohim-etz@users.noreply.github.com>
2026-08-23 22:08:52 +0530

    drivers: fix redefinition of __poll_t
    Signed-off-by: Naveen <133593113+elohim-etz@users.noreply.github.com>

--- drivers/misc/mediatek/gpu/gpu_mali/mali_bifrost/mali-r25p0/drivers/gpu/arm/midgard/mali_kbase_kinstr_jm.c
+++ drivers/misc/mediatek/gpu/gpu_mali/mali_bifrost/mali-r25p0/drivers/gpu/arm/midgard/mali_kbase_kinstr_jm.c
@@ -49,10 +49,6 @@
 #include <linux/wait.h>
 #include <linux/build_bug.h>
 
-#if KERNEL_VERSION(4, 16, 0) >= LINUX_VERSION_CODE
-typedef unsigned int __poll_t;
-#endif
-
 #ifndef ENOTSUP
 #define ENOTSUP EOPNOTSUPP
 #endif
